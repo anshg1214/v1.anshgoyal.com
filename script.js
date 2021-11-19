9fa96c714ba963fe8f3c61e9b36c18d08cb2b9ea
Ansh Goyal <anshg1214@gmail.com>
2021-11-19 16:49:21 +0530

    version 2

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if (window.scrollY > 20){
+            $('.navbar').addClass("sticky");   
+        }
+        else{
+            $('.navbar').removeClass("sticky");
+        }
+    });
+
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
     
+    var typed = new Typed(".typing-2", {
+        strings: ["Student", "Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+
+
+
+
+
+});
